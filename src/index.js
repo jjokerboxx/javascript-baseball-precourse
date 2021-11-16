07557b4bcfe5c23c00d8daf55cdc9620f9fc9920
jjokerboxx <dlrlgns58@gmail.com>
2021-11-16 22:57:40 +0900

    재시작 기능 추가, 0 생성 방지하기 + 점수 맞추기 기능 재설계하기

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@
 // 시작할 때 npx http-server로 실행해야함.
 
 export default function BaseballGame() {
+	this.isSolved = false;
+	this.createRestartBTN = () => {
+		let $restartBTN = document.createElement("button");
+		$restartBTN.id = "game-restart-button";
+		$restartBTN.innerText = "재시작";
+		document.querySelector("#result").appendChild($restartBTN);
+		document
+			.querySelector("#game-restart-button")
+			.addEventListener("click", (e) => {
+				this.restartGame();
+			});
+	};
+	this.checkAnswer = () => {
+		if (this.isSolved === true) {
+			this.createRestartBTN();
+		}
+	};
+	this.restartGame = () => {
+		this.isSolved = false;
+		this.setAnswer();
+		document.querySelector("#game-restart-button").remove();
+		document.querySelector("#user-input").value = "";
+		document.querySelector("#result").innerHTML = "";
+	};
 	// 0을 제거하는 방법 업데이트 하기!!
 	this.numberGenerator = () => {
 		const number = Math.floor(Math.random() * 10);
@@ -16,6 +40,7 @@ export default function BaseballGame() {
 		const firstDigit = this.numberGenerator().toString();
 		const secondDigit = this.numberGenerator().toString();
 		const thirdDigit = this.numberGenerator().toString();
+		console.log(`answer : ${firstDigit + secondDigit + thirdDigit}`);
 		return firstDigit + secondDigit + thirdDigit;
 	};
 
@@ -40,7 +65,10 @@ export default function BaseballGame() {
 				}
 			}
 		}
-		if (ballCounter !== 0 || strikeCounter !== 0) {
+		if (strikeCounter === 3) {
+			outcome = "3스트라이크";
+			this.isSolved = true;
+		} else if (ballCounter !== 0 || strikeCounter !== 0) {
 			outcome = `${ballCounter == 0 ? "" : ballCounter.toString() + "볼"} ${
 				strikeCounter == 0 ? "" : strikeCounter + "스트라이크"
 			}`;
@@ -49,6 +77,7 @@ export default function BaseballGame() {
 		}
 		return outcome;
 	};
+
 	this.printResult = (result) => {
 		document.querySelector("#result").innerHTML = result;
 	};
@@ -60,14 +89,14 @@ const init = () => {
 
 	const baseballGame = new BaseballGame();
 	const computerInput = baseballGame.setAnswer();
-	console.log(`answer : ${computerInput}`);
+	// console.log(`answer : ${computerInput}`);
 
 	document.querySelector("#submit").addEventListener("click", (e) => {
 		e.preventDefault();
 		const userInput = baseballGame.getUserInput();
 		const result = baseballGame.play(computerInput, userInput);
 		baseballGame.printResult(result);
-		console.log("clicked");
+		baseballGame.checkAnswer();
 	});
 };
 
